fbc1b458906b87dc5ecd83bf109c76b292229107
Matheus Ortiz <math.ortiz@outlook.com>
2023-05-16 17:47:13 -0300

    small changes

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const header = document.querySelector("header");
-window.addEventListener ("scroll", function(){
-    header.classList.toggle ("sticky", window.scrollY > 100);
-});
-
+let isHeaderClickable = true;
+const originalHeaderColor = "#FFD03A";
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.nav-list');
 
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", window.scrollY > 100);
+    header.style.backgroundColor = originalHeaderColor;
+});
+window.addEventListener ("click", function(){
+    isHeaderClickable = false;
+});
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
+    header.style.backgroundColor = "#FFD03A";
 }
 
 window.onscroll = () => {
