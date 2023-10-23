8de3c2902c9639f4022224277ed38333d9b36c29
Matheus Ortiz <math.ortiz@outlook.com>
2023-10-23 15:38:12 -0300

    Header sticky
    Alteração do header

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const header = document.querySelector("header");
+/*const header = document.querySelector("header");
 let isHeaderClickable = true;
-const originalHeaderColor = "#FFD03A";
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.nav-list');
 
 window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky", window.scrollY > 100);
-    header.style.backgroundColor = originalHeaderColor;
+    header.style.backgroundColor = "#FFD03A";
 });
 window.addEventListener ("click", function(){
     isHeaderClickable = false;
@@ -15,6 +14,24 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
     header.style.backgroundColor = "#FFD03A";
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}*/
+
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", window.scrollY > 100);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.nav-list');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
 }
 
 window.onscroll = () => {
